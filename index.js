34d2026fea3dc4adde3868a0716edf838d60b5ae
shrish-sharma-git <55692814+shrishsharma69@users.noreply.github.com>
2020-08-03 10:19:18 +0530

    make it better

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
 // NodeServer Which Will Handle SocketIO Connections.
-var PORT = process.env.PORT || 8000
+const cool = require('cool-ascii-faces');
+const express = require('express');
+const path = require('path');
+const PORT = process.env.PORT || 5000;
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
+  .get('/cool', (req, res) => res.send(cool()))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  
 const io = require('socket.io')(8000)
 
 const users = {};
